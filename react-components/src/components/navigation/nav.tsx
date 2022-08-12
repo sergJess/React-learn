dc3fe430abdef7ee6e43a560e2cf29a25170a779
sergJess <serg.fantasy@yandex.ru>
2022-08-12 21:08:16 +0300

    refactor: change code

--- react-components/src/components/navigation/nav.tsx
+++ react-components/src/components/navigation/nav.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import Header from '../header/header';
 import './nav.css';
 
+// interface NavProps {
+//   element: HTMLElement;
+// }
 class Nav extends React.Component {
+  // constructor(props: NavProps) {
+  //   super(props);
+  // }
   render(): JSX.Element {
     return (
       <nav className="nav">
         <Link className="nav-link" to="/">
           Home
         </Link>
-        <Link className="nav-link" to="/about-us">
+        <Link onClick={() => headerLog(<Header></Header>)} className="nav-link" to="/about-us">
           About US
         </Link>
         <Outlet />
@@ -18,3 +25,17 @@ class Nav extends React.Component {
   }
 }
 export default Nav;
+
+// function clickToAboutUs(element: HTMLElement): void {
+//   const elem = element;
+//   const parent = elem.parentNode;
+//   console.log(parent);
+//   if (parent) {
+//     const header = parent.parentNode as HTMLElement;
+//     header.classList.add('header-about-us');
+//   }
+// }
+
+function headerLog(el: JSX.Element) {
+  console.log(el);
+}
