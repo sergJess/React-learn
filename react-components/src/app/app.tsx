a3c2ee533bad7eee86c987fe2d364583678db941
sergJess <serg.fantasy@yandex.ru>
2022-08-12 19:31:56 +0300

    refactor: take out routing to app component

--- react-components/src/app/app.tsx
+++ react-components/src/app/app.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from '../pages/home';
+import ABoutUsPage from '../pages/about-us/about-us';
+import Page404 from '../pages/404/404';
+import Header from '../header/header';
 class App extends React.Component {
   render(): JSX.Element {
-    return <h1>Jess</h1>;
+    return (
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="about-us" element={<ABoutUsPage />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    );
   }
 }
 
--- react-components/src/index.tsx
+++ react-components/src/index.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/home';
-import ABoutUsPage from './pages/about-us/about-us';
-import Page404 from './pages/404/404';
-import Header from './header/header';
 import './index.css';
-
+import App from './app/app';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="about-us" element={<ABoutUsPage />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </BrowserRouter>
+    <App></App>
   </React.StrictMode>
 );
