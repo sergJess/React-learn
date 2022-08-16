0aeacb15e3ba1b3d654123703b55e8c5fc907937
sergJess <serg.fantasy@yandex.ru>
2022-08-16 08:19:13 +0300

    feat: add card

---
+++ react-components/src/components/card/card.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './card.css';
+type cardTitle = string;
+type cardImgSrc = string;
+type cardDescription = string;
+interface ICard {
+  title: cardTitle;
+  src: cardImgSrc;
+  description: cardDescription;
+}
+class Card extends React.Component<ICard> {
+  constructor(props: ICard) {
+    super(props);
+  }
+  render(): JSX.Element {
+    return (
+      <div className="card">
+        <h3 className="card-title">{this.props.title}</h3>
+        <div className="card-image-inner">
+          <img src={this.props.src} />
+        </div>
+        <p className="card-description">{this.props.description}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
---
+++ react-components/src/logicComonents/header/header-controller.tsx
@@ -0,0 +1,5 @@
+class HeaderController {
+  constructor() {}
+}
+
+export default HeaderController;
