730093ed4e223630a4f58eadac53c55e18b957ef
sergJess <serg.fantasy@yandex.ru>
2022-08-17 22:16:34 +0300

    feat: add input event

--- react-components/src/components/search/search.tsx
+++ react-components/src/components/search/search.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './search.css';
 class Search extends React.Component {
+  constructor(props = {}) {
+    super(props);
+    this.state = { value: '' };
+  }
   handleClick = () => {
     console.log('значение this:', this);
+  };
+  inputEvent = (value: string) => {
+    this.setState({ value: `${value}` });
   };
   setBackground(): void {}
   render(): JSX.Element {
     return (
       <div className="search">
-        <input type="text" className="search-input" onClick={this.handleClick} />
+        <input
+          type="text"
+          className="search-input"
+          onClick={this.handleClick}
+          onInput={this.inputEvent.bind(this, e:React.ChangeEvent<HTMLInputElement>)}
+        />
       </div>
     );
   }
