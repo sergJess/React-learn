0f810684cbd855457fbf9fc13d8154b439befc5d
sergJess <serg.fantasy@yandex.ru>
2022-08-18 07:52:08 +0300

    feat: add well working search loop

--- react-components/src/components/search/search.tsx
+++ react-components/src/components/search/search.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import './search.css';
 class Search extends React.Component {
+  inputClasses: string;
   constructor(props = {}) {
     super(props);
     this.state = { value: '' };
+    this.inputEvent.bind(this);
+    this.handleClick.bind(this);
+    this.inputClasses = 'search-input  search-loop-visible';
   }
   handleClick = () => {
     console.log('значение this:', this);
   };
-  inputEvent = (value: string) => {
-    this.setState({ value: `${value}` });
+  inputEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event) {
+      this.setState({ value: `${event.target.value}` });
+      if (!event.target.value.length) {
+        event.target.classList.remove('search-loop-invisible');
+        event.target.classList.add('search-loop-visible');
+      } else {
+        event.target.classList.remove('search-loop-visible');
+        event.target.classList.add('search-loop-invisible');
+      }
+    }
   };
-  setBackground(): void {}
   render(): JSX.Element {
     return (
       <div className="search">
         <input
           type="text"
-          className="search-input"
+          className={this.inputClasses}
           onClick={this.handleClick}
-          onInput={this.inputEvent.bind(this, e:React.ChangeEvent<HTMLInputElement>)}
+          onChange={this.inputEvent}
         />
       </div>
     );
